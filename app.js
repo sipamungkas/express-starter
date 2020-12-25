8dcdf5cba0664b5d8384deed8d092734364cd58f
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2020-12-26 06:56:38 +0700

    cek

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
--- lint-staged.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    linters: {
-        '**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|scss|json|vue)': [
-          'eslint --fix',
-          'prettier --write',
-          'git add',
-        ],
-      },
-}
