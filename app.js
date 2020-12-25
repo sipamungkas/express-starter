d18535429ce88cad79d84abba779946ea9a0ff67
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2020-12-26 06:53:43 +0700

    tes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
+var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 const indexRouter = require('./routes/index');
