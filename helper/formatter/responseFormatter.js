d2b0f82a7240954d687644b618ebe14b3174cfaa
Ragil Burhanudin Pamungkas <om.ragil7@gmail.com>
2021-01-10 18:17:27 +0700

    feat: added users/session unit test

--- helper/formatter/responseFormatter.js
+++ helper/formatter/responseFormatter.js
@@ -8,7 +8,7 @@ const meta = (message, code, status) => {
 };
 
 const formatRes = (metaData, data) => {
-  const apiResponse = {metaData, data};
+  const apiResponse = {meta: metaData, data};
   return apiResponse;
 };
 
--- test/index.js
+++ test/index.js
@@ -18,4 +18,21 @@ describe('API Test', () => {
         done();
       });
   });
+  it('should GET /v1/users/session', (done) => {
+    chai
+      .request(app)
+      .post('/v1/users/session')
+      .send({email: 'ragil@sipamungkas.com', password: '123'})
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.be.not.empty;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an('object');
+        expect(res.body).to.have.property('meta');
+        expect(res.body.meta).to.have.all.keys('message', 'code', 'status');
+        expect(res.body).to.have.property('data');
+        expect(res.body.data).to.have.all.keys('id', 'name', 'email', 'token');
+        done();
+      });
+  });
 });
